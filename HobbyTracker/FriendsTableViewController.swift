3c25f870cc866b409ccc3e678099850f8761f916
Breanna Young <bree.j.32@gmail.com>
2020-02-26 14:49:18 -0500

    added navigation controller, tableview controller and add view controller

--- HobbyTracker/FriendsTableViewController.swift
+++ HobbyTracker/FriendsTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FriendsTableViewController.swift
 //  HobbyTracker
 //
 //  Created by Bree Jeune on 2/26/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FriendsTableViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
